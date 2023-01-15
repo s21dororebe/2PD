84500c016ca1c004b75c04867b966f0c228b9b62
s21dororebe <s21dororebe@venta.lv>
2023-01-16 00:12:07 +0200

    Code has been optimized Added function for uploding images

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -5,8 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Author;
 use Illuminate\Http\Request;
 use App\Models\Book;
-
-//use App\Http\Requests\BookRequest;
+use App\Http\Requests\BookRequest;
 
 class BookController extends Controller
 {
@@ -22,40 +21,16 @@ class BookController extends Controller
         return view('book.form', ['title' => 'Pievienot grāmatu', 'book' => new Book(), 'authors' => $authors,]);
     }
 
-    public function put(Request $request)
-    {
-        $validatedData = $request->validate(['name' => 'required|min:3|max:256', 'author_id' => 'required', 'description' => 'nullable', 'price' => 'nullable|numeric', 'year' => 'numeric', 'image' => 'nullable|image', 'display' => 'nullable']);
-        $book = new Book();
-        $book->name = $validatedData['name'];
-        $book->author_id = $validatedData['author_id'];
-        $book->description = $validatedData['description'];
-        $book->price = $validatedData['price'];
-        $book->year = $validatedData['year'];
-        $book->display = (bool)($validatedData['display'] ?? false);
-        if ($request->hasFile('image')) {
-            $uploadedFile = $request->file('image');
-            $extension = $uploadedFile->clientExtension();
-            $name = uniqid();
-            $book->image = $uploadedFile->storePubliclyAs('/', $name . '.' . $extension, 'uploads');
-        }
-        $book->save();
-        return redirect('/books');
-    }
-
     public function update(Book $book)
     {
         $authors = Author::orderBy('name', 'asc')->get();
         return view('book.form', ['title' => 'Rediģēt grāmatu', 'book' => $book, 'authors' => $authors,]);
     }
 
-    public function patch(Book $book, Request $request)
+    private function saveBookData(Book $book, BookRequest $request)
     {
-        $validatedData = $request->validate(['name' => 'required|min:3|max:256', 'author_id' => 'required', 'description' => 'nullable', 'price' => 'nullable|numeric', 'year' => 'numeric', 'image' => 'nullable|image', 'display' => 'nullable']);
-        $book->name = $validatedData['name'];
-        $book->author_id = $validatedData['author_id'];
-        $book->description = $validatedData['description'];
-        $book->price = $validatedData['price'];
-        $book->year = $validatedData['year'];
+        $validatedData = $request->validated();
+        $book->fill($validatedData);
         $book->display = (bool)($validatedData['display'] ?? false);
         if ($request->hasFile('image')) {
             $uploadedFile = $request->file('image');
@@ -64,6 +39,18 @@ class BookController extends Controller
             $book->image = $uploadedFile->storePubliclyAs('/', $name . '.' . $extension, 'uploads');
         }
         $book->save();
+    }
+
+    public function put(BookRequest $request)
+    {
+        $book = new Book();
+        $this->saveBookData($book, $request);
+        return redirect('/books');
+    }
+
+    public function patch(Book $book, BookRequest $request)
+    {
+        $this->saveBookData($book, $request);
         return redirect('/books/update/' . $book->id);
     }
 
